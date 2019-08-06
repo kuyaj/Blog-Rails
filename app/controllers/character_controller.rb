0b428dde644a9336c2c20c0779a00e3425d3cc94
unknown <valmocinajayangelo@gmail.com>
2019-08-06 17:29:24 +0800

    Created show, delete feature, but still lacking a way to edit and post

--- app/controllers/character_controller.rb
+++ app/controllers/character_controller.rb
@@ -1,4 +1,16 @@
 class CharacterController < ApplicationController
   def index
+    @character = Character.all
   end
-end
+
+  def show
+    @character = Character.find(params[:id])
+    render :show
+  end
+
+  def destroy
+    @character = Character.find(params[:id])
+    @character.destroy
+    redirect_to root_path, notice: "Your post have been deleted!"
+  end
+end   
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root 'character#index'
+  get 'character/:id', to: 'character#show', as: :show_post
+  delete "character/:id", to: 'character#destroy', as: :delete_post
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
